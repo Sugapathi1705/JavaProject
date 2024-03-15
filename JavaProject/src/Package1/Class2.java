7e6c9a33ac18fcf50689f43e3b34436ebeef5774
DELL <DELL@DESKTOP-D0KND1R>
2024-03-15 20:49:45 +0530

    this about the complete constructor program

--- JavaProject/src/Package1/Class2.java
+++ JavaProject/src/Package1/Class2.java
@@ -1,7 +1,8 @@
 package Package1;
 
-public class Class1 {
+public class Class2 {
 	public static void main(String args[]) {
-		 System.out.println("Welcome all");
+		System.out.println("Good Evening");
 	}
+
 }
